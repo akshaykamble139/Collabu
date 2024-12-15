b36cd19848b6d7c9edbf6b9cbbad9c682b3aa1bf
akshaykamble139 <kakshayiitk@gmail.com>
2024-12-16 02:29:46 +0530

    v0.01
    Spring boot project created, database user configured and db connection tested by running the application

---
+++ src/main/java/com/akshay/Collabu/CollabuApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.Collabu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/Collabu/CollabuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.Collabu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
